82077bfc17eb65dccba0c677806031f63caceb3e
MohamedDer <medder12@gmail.com>
2018-05-30 18:12:37 +0000

    initial commit

---
+++ TableView Sorting/Models/Employee.swift
@@ -0,0 +1,36 @@
+//
+//  Employee.swift
+//  TableView Sorting
+//
+//  Created by Mohamed Derkaoui on 30/05/2018.
+//  Copyright © 2018 Mohamed Derkaoui. All rights reserved.
+//
+
+import Foundation
+
+
+class Employee  {
+    
+    private let name: String
+    private let birthYear: Int
+    private var salary: Int?
+    
+    private let startingSalary = 1000
+    private let salaryCurrency = "EUR"
+    
+    init(name: String, birthYear: Int) {
+        self.name = name
+        self.birthYear = birthYear
+        self.salary = startingSalary
+    }
+    
+    func getName() -> String { return self.name }
+    func getBirthYear() -> Int { return self.birthYear }
+    func getSalary() -> String {
+        let locale = NSLocale(localeIdentifier: self.salaryCurrency)
+        let currencySymbol = locale.displayName(forKey: NSLocale.Key.currencySymbol, value: self.salaryCurrency)!
+        return "\(self.salary!) \(currencySymbol)"
+    }
+    
+    func setSalary(newSalary: Int) { self.salary = newSalary }
+}
---
+++ TableView Sorting/Models/EmployeeDirectory.swift
@@ -0,0 +1,54 @@
+//
+//  EmployeeDirectory.swift
+//  TableView Sorting
+//
+//  Created by Mohamed Derkaoui on 30/05/2018.
+//  Copyright © 2018 Mohamed Derkaoui. All rights reserved.
+//
+
+import Foundation
+
+
+class EmployeeDirectory: NSObject {
+    
+    var employees: [Employee]?
+    var isUpdating = false
+    
+    
+    func update(){
+        if isUpdating {
+            return
+        } else {
+            isUpdating = true
+            DispatchQueue.global(qos: .background).async {
+                self.updateEmployeesInTheBackground()
+            }
+        }
+    }
+    
+    
+    func updateEmployeesInTheBackground() {
+        sleep(2)
+        let names = ["Anne", "Lucas", "Marc", "Zeus", "Hermes", "Bart", "Paul", "John","Ringo", "Dave", "Taylor"]
+        let surnames = ["Hawkins", "Simpson", "Lennon", "Grohl", "Hawkins", "Jacobs",
+                        "Holmes", "Mercury", "Matthews"]
+        let amount = names.count*surnames.count
+        var employees = [Employee]()
+        for _ in 1...amount {
+            let fullName = "\(names[Int(arc4random_uniform(UInt32(names.count)))]) \(surnames[Int(arc4random_uniform(UInt32(surnames.count)))])"
+            employees.append(Employee(name: fullName, birthYear: 1997 - Int(arc4random_uniform(50))))
+        }
+        DispatchQueue.main.async {
+            self.updateDidFinishWithResults(employees: employees)
+        }
+        
+    }
+    
+    
+    func updateDidFinishWithResults(employees: [Employee]) {
+        self.employees = employees
+        self.isUpdating = false
+    }
+
+    
+}
