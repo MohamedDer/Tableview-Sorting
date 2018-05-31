1352f3679a1ee7c23926f7170d916f91f547c733
MohamedDer <medder12@gmail.com>
2018-05-31 19:29:27 +0000

    custom layout for each cell

--- TableView Sorting/Models/EmployeeDirectory.swift
+++ TableView Sorting/Models/EmployeeDirectory.swift
@@ -7,8 +7,6 @@
 //
 
 import Foundation
-import PromiseKit
-
 
 class EmployeeDirectory: NSObject {
     
--- TableView Sorting/Views/EmployeeCell.swift
+++ TableView Sorting/Views/EmployeeCell.swift
@@ -13,6 +13,8 @@ class EmployeeCell: UITableViewCell {
     let nameLabel = UILabel()
     let salaryLabel = UILabel()
     let birthyearLabel = UILabel()
+    
+    let backgroundColors = [UIColor.red,UIColor.green,UIColor.yellow,UIColor.orange,UIColor.blue,UIColor.brown]
 
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:)")
@@ -21,13 +23,31 @@ class EmployeeCell: UITableViewCell {
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        nameLabel.frame = CGRect(x: 5, y: 5, width: 190, height: 20)
-        salaryLabel.frame = CGRect(x: 250, y: 5, width: 150, height: 20)
-        birthyearLabel.frame = CGRect(x: 5, y: 35, width: 150, height: 20)
-
         contentView.addSubview(nameLabel)
         contentView.addSubview(salaryLabel)
         contentView.addSubview(birthyearLabel)
+        
+        self.backgroundColor = backgroundColors[Int(arc4random_uniform(UInt32(backgroundColors.count)))]
+        
+        let randomStyle = Int(arc4random_uniform(3))
+        
+        switch randomStyle {
+            case 0:
+                nameLabel.frame = CGRect(x: 5, y: 5, width: 190, height: 20)
+                salaryLabel.frame = CGRect(x: 250, y: 5, width: 150, height: 20)
+                birthyearLabel.frame = CGRect(x: 5, y: 35, width: 150, height: 20)
+            case 1:
+                nameLabel.frame = CGRect(x: 120, y: 5, width: 270, height: 20)
+                salaryLabel.frame = CGRect(x: 210, y: 35, width: 150, height: 20)
+                birthyearLabel.frame = CGRect(x: 25, y: 35, width: 150, height: 20)
+                nameLabel.font = UIFont(name: "Helvetica", size: 22)
+            default:
+                nameLabel.frame = CGRect(x: 5, y: 2, width: 190, height: 20)
+                salaryLabel.frame = CGRect(x: 5, y: 22, width: 150, height: 20)
+                birthyearLabel.frame = CGRect(x: 5, y: 41, width: 150, height: 20)
+            
+        
+        }
         
     }
     
